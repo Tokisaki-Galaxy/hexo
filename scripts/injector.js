45202f8dc29ec06effc02773279cf2bbe38f9ec1
Tokisaki-Galaxy <moebest@outlook.jp>
2025-03-13 18:41:30 +0800

    更新依赖项CDN

---
+++ scripts/injector.js
@@ -0,0 +1,3 @@
+hexo.extend.injector.register('body_end'
+    , '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/TrymenT-ClocK/TrymenT-ClocK.min.css"><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/TrymenT-ClocK/TrymenT-ClocK.min.js"></script>'
+    , 'default');
